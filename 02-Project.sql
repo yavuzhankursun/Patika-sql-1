b1c4713712a0d9ec8e3b6b9860d052d58bb20acd
Yavuzhan Kurşun <96658678+yavuzhankursun@users.noreply.github.com>
2023-08-16 01:48:35 +0300

    Update and rename 02-Project.md to 02-Project.sql

---
+++ 02-Project.sql
@@ -0,0 +1,11 @@
+-- Question 1: Retrieve all data from the 'film' table where the replacement cost is greater than or equal to 12.99 and less than 16.99 using the BETWEEN - AND structure.
+-- ANSWER: 
+      SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+
+-- Question 2: Retrieve the data from the 'first_name' and 'last_name' columns in the 'actor' table where the 'first_name' is 'Penelope' or 'Nick' or 'Ed' using the IN operator.
+-- ANSWER: 
+      SELECT first_name, last_name FROM actor WHERE first_name IN('Penelope', 'Nick', 'Ed');
+
+-- Question 3: Retrieve all data from the 'film' table where the 'rental_rate' is 0.99, 2.99, or 4.99 AND the 'replacement_cost' is 12.99, 15.99, or 28.99 using the IN operator.
+-- ANSWER: 
+      SELECT * FROM film WHERE rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);
